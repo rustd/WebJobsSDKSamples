cfae77b11a3e69cab3e47b09d4e86730a3a8a226
rustd <raspranav@gmail.com>
2014-07-15 11:22:33 -0700

    Samples for Azure WebJobs

---
+++ ProcessImage/Program.cs
@@ -0,0 +1,33 @@
+﻿using nQuant;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessImage
+{
+    // To learn more about Microsoft Azure WebJobs, please see http://go.microsoft.com/fwlink/?LinkID=401557
+    class Program
+    {
+        static void Main()
+        {
+        }
+        public static void SquishNewlyUploadedPNGs(
+            Stream input, 
+            Stream output)
+        {
+            var quantizer = new WuQuantizer();
+            using (var bitmap = new Bitmap(input))
+            {
+                using (var quantized = quantizer.QuantizeImage(bitmap))
+                {
+                    quantized.Save(output, ImageFormat.Png);
+                }
+            }
+        }
+    }
+}
---
+++ ProcessImageInCloud/Program.cs
@@ -0,0 +1,37 @@
+﻿using ImageProcessor;
+using Microsoft.Azure.Jobs;
+using nQuant;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessImageInCloud
+{
+    // To learn more about Microsoft Azure WebJobs, please see http://go.microsoft.com/fwlink/?LinkID=401557
+    class Program
+    {
+        static void Main()
+        {
+            JobHost host = new JobHost();
+            host.RunAndBlock();
+        }
+        public static void SquishNewlyUploadedPNGs(
+            [BlobTrigger("processimageincloud/{name}")] Stream input,
+            [Blob("processimageincloudoutput/{name}", FileAccess.Write)] Stream output)
+        {
+            var quantizer = new WuQuantizer();
+            using (var bitmap = new Bitmap(input))
+            {
+                using (var quantized = quantizer.QuantizeImage(bitmap))
+                {
+                    quantized.Save(output, ImageFormat.Png);
+                }
+            }
+        }
+    }
+}
